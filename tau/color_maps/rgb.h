01eeb481f383ea4e4adf6b65c7053a7e72bd4bb9
Jive Helix <jivehelix@gmail.com>
2023-10-07 19:54:37 -0600

    Moved RgbPixels to tau/color.h.

--- tau/color_maps/rgb.h
+++ tau/color_maps/rgb.h
@@ -26,38 +26,5 @@ RgbMatrix<T> MakeRgb(size_t count, MakeFloat makeFloat)
     return converted;
 }
 
-template<typename T>
-struct RgbPixels
-{
-    using Data = RgbMatrix<T>;
-    using Index = Eigen::Index;
-
-    Data data;
-
-    // The PixelMatrix is stored as a count x 3 matrix, where each row contains
-    // an RGB triplet.
-    // height and width store the dimensions the image.
-    // Expect height * width == data.rows()
-    Size<Index> size;
-
-    static RgbPixels<T> Create(const Size<Index> &size)
-    {
-        RgbPixels<T> result;
-        result.size = size;
-        result.data = Data::Zero(size.height * size.width, 3);
-
-        return result;
-    }
-
-    static std::shared_ptr<RgbPixels<T>> CreateShared(const Size<Index> &size)
-    {
-        auto result = std::make_shared<RgbPixels<T>>();
-        result->size = size;
-        result->data = Data::Zero(size.height * size.width, 3);
-
-        return result;
-    }
-};
-
 
 } // end namespace tau
